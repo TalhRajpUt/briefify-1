88d9cdaa6596bfd90c3ef99ce53ab1c3d0795ccb
Talha <TalhaRajpUt>
2022-03-08 17:24:09 +0500

    IOS setting Update

--- lib/main.dart
+++ lib/main.dart
@@ -88,27 +88,29 @@ class MyApp extends StatelessWidget {
           // Profile Picture Display Route
 
           if (settings.name == ProfileeImgScreen) {
-            return MaterialPageRoute(builder: (context) => const ProfileImgScreen());
+            return MaterialPageRoute(
+                builder: (context) => const ProfileImgScreen());
           }
 
           // Other User Cover Picture Display Route
           if (settings.name == OtherUserCoverImg) {
             final results = settings.arguments as Map;
             var modelSendToshowimg = results['user'];
-            return MaterialPageRoute(builder: (context) => ShowOtherUserCoverPhoto(
-                user: modelSendToshowimg,
-            )
-            );
+            return MaterialPageRoute(
+                builder: (context) => ShowOtherUserCoverPhoto(
+                      user: modelSendToshowimg,
+                    ));
           }
           // Other User Profile Picture Display Route
           if (settings.name == OtherUserProfileImg) {
             final results = settings.arguments as Map;
             var user = results['user'];
-            return MaterialPageRoute(builder: (context) => OtherUserProfilePhoto(
-              user: user,
-            )
-            );
+            return MaterialPageRoute(
+                builder: (context) => OtherUserProfilePhoto(
+                      user: user,
+                    ));
           }
+
           /// Register Route
           if (settings.name == registerRoute) {
             return MaterialPageRoute(
@@ -147,8 +149,8 @@ class MyApp extends StatelessWidget {
           /// Edit Post Route
           if (settings.name == editPostRoute) {
             final results = settings.arguments as EditPostArgument;
-            int  userId = results.userId ?? 0;
-            int  postId = results.postId ?? 0;
+            int userId = results.userId ?? 0;
+            int postId = results.postId ?? 0;
             String heading = results.heading ?? " ";
             String summary = results.summary ?? " ";
             String videolink = results.videolink ?? " ";
@@ -156,14 +158,14 @@ class MyApp extends StatelessWidget {
             // String category = results.category ?? "";
             return MaterialPageRoute(
                 builder: (context) => EditPostScreen(
-                    userId: userId,
-                    postId: postId,
-                    heading: heading,
-                    summary: summary,
-                    videolink: videolink,
-                    ariclelink: ariclelink,
-                    // category: category,
-                ));
+                      userId: userId,
+                      postId: postId,
+                      heading: heading,
+                      summary: summary,
+                      videolink: videolink,
+                      ariclelink: ariclelink,
+                      // category: category,
+                    ));
           }
 
           /// My Profile Route
@@ -233,8 +235,8 @@ class MyApp extends StatelessWidget {
             UserModel user = results['user'];
             return MaterialPageRoute(
                 builder: (context) => ShowUserScreen(
-                    user: user,
-                ));
+                      user: user,
+                    ));
           }
 
           /// Followers Route
@@ -273,14 +275,14 @@ class MyApp extends StatelessWidget {
 
           // Report User Route
           if (settings.name == reportUserRoute) {
-          final results = settings.arguments as Map;
-          final int postid = results['postid'];
-          final int postuserid = results['userid'];
-            return MaterialPageRoute(builder: (context) => ReportUser(
-                postid: postid,
-                userid: postuserid,
-            )
-            );
+            final results = settings.arguments as Map;
+            final int postid = results['postid'];
+            final int postuserid = results['userid'];
+            return MaterialPageRoute(
+                builder: (context) => ReportUser(
+                      postid: postid,
+                      userid: postuserid,
+                    ));
           }
 
           /// No route found
@@ -288,91 +290,6 @@ class MyApp extends StatelessWidget {
           return null;
         },
       ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
